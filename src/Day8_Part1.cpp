8e41bb26e4c8478fcd470c10d6f18c1b73470081
stch27113 <extern.Stamatis.Chiotis@elektrobit.com>
2020-02-19 15:09:07 +0100

    Day8_Part1

--- src/Day8_Part1.cpp
+++ src/Day8_Part1.cpp
@@ -10,117 +10,81 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <iterator>
 #include <exception>
+#include <algorithm>
 
-
-using Layers = std::vector<std::vector<std::vector<char>>>;
-using Layer = std::vector<std::vector<char>>;
-static constexpr size_t WIDE = 3;
-static constexpr size_t TALL = 2;
+using Layers = std::vector <std::string>;
+static constexpr size_t WIDE = 25;
+static constexpr size_t TALL = 6;
 static constexpr char FILENAME[] = "../../../resources/Day8.txt";
+
+
+static inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
 
 Layers ReadFile()
 {
-	Layers layers;
-	Layer layer;
-	std::vector<char> row;
-	char ch;
-	std::ifstream input(FILENAME);
-	
-	input.seekg (0, input.end);
-	int length = input.tellg();
-	input.seekg(0, input.beg);
-	int counter = 0;
+    Layers layers;
+    std::ifstream input(FILENAME);
 
 	if (input.is_open())
 	{
-		while(1)
-		{
-		
-		for (int tall = 0; tall < TALL; tall++)
-		{
-			for (int wide = 0; wide < WIDE; wide++)
-			{
-				if(length == counter)
-				{
-					break;
-				}
-				input >> std::skipws >> ch;
-				row.emplace_back(ch);
-				++counter;
-			}
-			layer.emplace_back(row);
-			if(length == counter)
-			{
-				break;
-			}
-		}
-		if(length == counter)
-		{
-			break;
-		}
-		}
-		layers.emplace_back(layer);
-		input.close();
+        std::string content( (std::istreambuf_iterator<char>(input) ),
+                             (std::istreambuf_iterator<char>()    ) );
+
+        for (int i = 0; i < content.size(); i += WIDE * TALL)
+        {
+            std::string line = content.substr(i, WIDE * TALL);
+            ltrim(line);
+
+            if(!line.empty())
+            {
+                layers.emplace_back(line);
+            }
+        }
+        input.close();
 	}
-		
-// 		input.seekg (0, input.end);
-// 		int length = input.tellg();
-// 		input.seekg(0, input.beg);
-// 		int counter = 0;
-// 		std::cout << "before" <<std::endl;
-// 		for (int tall = 0; tall < TALL; tall++)
-// 		{
-// 			std::cout << "in first" <<std::endl;
-// 			for (int wide = 0; wide < WIDE; wide++)
-// 			{		
-// 				
-// 				
-// 				
-// 				input.read(ch, 1);
-// 				std::cout << "in second" <<std::endl;
-// 				row.emplace_back(ch);
-// 				++counter;
-// 			}
-// 			layer.emplace_back(row);
-// 			if (counter == length)
-// 			{
-// 				break;
-// 			}
-// 		}
-// 		input.close();
-// 	}
 	else
 	{
 		std::string exception_string =  "Could not open input file";
 		throw std::runtime_error(exception_string);
 	}
+
 	return layers;
 }
 
 void PrintLayers(Layers layers)
 {
-	for(auto& layer : layers)
-	{
-		std::cout << "Layers: ";
-		for(auto& row : layer)
-		{
-			std::cout << "Row 1: ";
-			for (auto& ch : row)
-			{	
-				std::cout << ch;
-			}
-			std::cout << std::endl;
-		}
-		std::cout << std::endl;
-	}
+    int counter = 0;
+    for (auto x : layers)
+    {
+        std::cout << "Layer " << counter << ": " << x << std::endl;
+        counter++;
+    }
+}
+
+int CountZeros(Layers layers)
+{
+    int count_zeros = 10000, res;
+
+    for (auto& layer : layers)
+    {
+        int temp = std::count(layer.begin(), layer.end(), '0');
+        if (count_zeros > temp)
+        {
+            count_zeros = temp;
+            res = std::count(layer.begin(), layer.end(), '1') * std::count(layer.begin(), layer.end(), '2');
+        }
+    }
+    return res;
 }
 
 int main()
 {
-	
-	Layers layers;
+    Layers layers;
 	try
 	{	
 		layers = ReadFile();
@@ -129,9 +93,10 @@ int main()
 	{
 		std::cout << exception_string << std::endl;
 	}
-	
-	PrintLayers(layers);
-	
-	
+
+
+	int res =  CountZeros(layers);
+    std::cout << "Result is: " << res << std::endl;
+
 	return 0;
 }
