3072deec4ed937429a7ea33010b1350255e622a6
stam1989 <chstamatis21@gmail.com>
2020-03-02 14:21:16 +0200

    Day 10

--- src/Day10_Part1.cpp
+++ src/Day10_Part1.cpp
@@ -10,11 +10,13 @@
 #include <fstream>
 #include <string>
 #include <exception>
-
+#include <algorithm>
+#include <math.h>
 
 using Coords = std::vector<std::pair<int, int>>;
 
 static constexpr char FILENAME[] = "../../../resources/Day10.txt";
+
 
 void ReadFile(Coords& coords)
 {
@@ -53,36 +55,80 @@ void ReadFile(Coords& coords)
 	}
 }
 
-bool IfBlocked(std::pair<int, int>& possible_station, std::pair<int, int>& point_to_check, Coords& Coords)
-{
-    if()
-}
 
-int GetNumber(std::pair<int, int>& possible_station, Coords& coords)
+int GetNumber(std::pair<int, int>& possible_station, Coords coords)
 {
     Coords asteroids_detected;
-    for (auto &point : coords)
-    {
-        if (point == possible_station)
-        {
-            continue;
-        }
-
-        if (!IfBlocked(possible_station, point, asteroids_detected)
-        {
-            asteroids_detected.emplace_back(point);
-        }
+    for (auto i = 0; i < coords.size(); i++)
+    {		
+		if (coords[i] == possible_station)
+		{
+			continue;
+		}
+		
+		if (asteroids_detected.empty())
+		{
+			asteroids_detected.emplace_back(coords[i]);
+			continue;
+		}
+		
+		bool intersection_flag = false;
+		for (auto j = 0; j < asteroids_detected.size(); j++)
+		{
+			if(std::atan2(possible_station.first - asteroids_detected[j].first, possible_station.second - asteroids_detected[j].second) == 
+				std::atan2(possible_station.first - coords[i].first, possible_station.second - coords[i].second))
+			{
+				intersection_flag = true;
+				
+				if(abs(possible_station.second - asteroids_detected[j].second) < abs(possible_station.second - coords[i].second))
+				{
+					std::swap(asteroids_detected[j], coords[i]);
+				}
+				
+				break;
+			}
+		}
+		
+		if (!intersection_flag)
+		{
+			asteroids_detected.emplace_back(coords[i]);
+		}
+		
     }
-    return asteroid_detected.size();
+    return asteroids_detected.size();
 }
 
-std::pair<int, int> FindStation(Coords coords)
+
+void FindStation(Coords coords)
 {
     std::vector<int> asteroid_num;
     for(auto &possible_station : coords)
     {
         asteroid_num.emplace_back(GetNumber(possible_station, coords));
     }
+    
+    auto max_ast_pos = std::max_element(asteroid_num.begin(), asteroid_num.end());
+	int pos = std::distance(asteroid_num.begin(), max_ast_pos);
+	
+	std::cout << "The station should be built in: (" << coords[pos].first << " ," << coords[pos].second << ") with " << *max_ast_pos << " asteroids detected!\n";
+}
+
+struct Point
+{
+	int x, y, slope, position
+}
+
+void VaporizeAsteroid(Coords coords, std::pair<int, int> station)
+{
+	std::vector<std::pair<int, int>> slopes;   // vector of pairs of sloped and point's position in coords
+	for (auto i = 0; i < coords.size(); i++)
+	{
+		if (coords[i] == station)
+		{
+			continue;
+		}
+		slope.emplace_back(std::atan2(), i);
+	}
 }
 
 
@@ -102,7 +148,9 @@ int main()
 	ReadFile(coords);
 	
 	PrintCoords(coords);
+	FindStation(coords);
 	
+// 	std::cout << std::atan2(0, 1) << ", " << std::atan2(1,0) << std::endl;
 	
 	return 0;
 }
