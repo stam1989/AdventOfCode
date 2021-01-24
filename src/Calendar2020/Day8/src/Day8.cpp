157d94db6f3a9c92896e8df0cc2335064c0c58fb
stch27113 <extern.Stamatis.Chiotis@elektrobit.com>
2021-01-24 20:02:58 +0100

    Implement Day8

--- src/Calendar2020/Day8/src/Day8.cpp
+++ src/Calendar2020/Day8/src/Day8.cpp
@@ -29,7 +29,7 @@ void Day8::ReadFile()
             int step;
             ss >> opcode;
             ss >> step;
-            m_opcodes.emplace_back(opcode, step);
+            m_instructions.emplace_back(StringToOpcode(opcode), step);
         }
         inputFile.close();
     }
@@ -42,20 +42,136 @@ void Day8::ReadFile()
     std::cout << "ReadFile finished\n";
 }
 
-void Day8::PrintOpcodes()
+
+Day8::Opcode Day8::StringToOpcode(const std::string& op)
 {
-    for (const auto& op : m_opcodes)
+    if(op == "acc")
     {
-        std::cout << op.first << ": " << op.second << "\n";
+        return Opcode::ACC;
+    }
+    else if(op == "jmp")
+    {
+        return Opcode::JMP;
+    }
+    else if(op == "nop")
+    {
+        return Opcode::NOP;
+    }
+    else
+    {
+        throw std::invalid_argument("GetOpcode:: Invalid opcode...");
     }
 }
+
+
+std::string Day8::OpcodeToString(const Opcode& op)
+{
+    if(op == Opcode::ACC)
+    {
+        return "acc";
+    }
+    else if(op == Opcode::JMP)
+    {
+        return "jmp";
+    }
+    else if(op == Opcode::NOP)
+    {
+        return "nop";
+    }
+    else
+    {
+        throw std::invalid_argument("GetOpcode:: Invalid opcode...");
+    }
+}
+
+
+void Day8::PrintOpcodes()
+{
+    for (const auto& inst : m_instructions)
+    {
+        std::cout << OpcodeToString(inst.op) << ": " << inst.step << "\n";
+    }
+}
+
+
+bool Day8::Operation(int& accumulator, Instructions instructions)
+{
+    int ip = 0;
+    while (1)
+    {
+        if(instructions[ip].isVisited == true)
+        {
+            return false;
+        }
+        if(ip >= instructions.size())
+        {
+            return true;
+        }
+
+        instructions[ip].isVisited = true;
+
+        switch (instructions[ip].op)
+        {
+        case Opcode::ACC:
+            accumulator += instructions[ip].step;
+            ip++;
+            break;
+        case Opcode::JMP:
+            ip += instructions[ip].step;
+            break;
+        case Opcode::NOP:
+            ip++;
+            break;
+        default:
+            throw std::invalid_argument("Operation:: Invalid opcode...");
+            break;
+        }
+    }   
+}
+
+
+Day8::Instructions Day8::ChangeInstruction(int& ip)
+{
+    Instructions res_instructions(m_instructions);
+
+    for (int i = ip; i < res_instructions.size(); i++)
+    {
+        if (res_instructions[i].op == Opcode::JMP)
+        {
+            res_instructions[i].op = Opcode::NOP;
+            ip++;
+            return res_instructions;
+        }
+        else if (res_instructions[i].op == Opcode::NOP && res_instructions[i].step != 0)
+        {
+            res_instructions[i].op = Opcode::JMP;
+            ip++;
+            return res_instructions;
+        }
+
+    }
+}
+
 
 
 int main()
 {
     Day8 d;
 
-    d.PrintOpcodes();
+    // d.PrintOpcodes();
+    int acc = 0;
+    auto instr = d.GetInstructions();
+    d.Operation(acc, instr);
+    std::cout << "The value in the accumulator is: " << acc << "\n";
+
+    int ip = 0;
+    while (!d.Operation(acc, instr))
+    {
+        acc = 0;
+        instr = d.ChangeInstruction(ip);
+    } 
+    std::cout << "The value in the accumulator after the change is: " << acc << "\n";
+    
 
     return 0;
 }
--- src/Calendar2020/Day8/src/Day8.h
+++ src/Calendar2020/Day8/src/Day8.h
@@ -21,17 +21,37 @@ public:
 
     enum class Opcode
     {
-        acc, jmp, nop
+        ACC,    // increases or decreases a single global value called the accumulator by the value given in the argument
+        JMP,    // jumps to a new instruction relative to itself. The next instruction to execute is found using the argument as an offset from the jmp instruction
+        NOP     // stands for No OPeration - it does nothing. The instruction immediately below it is executed next.
     };
 
-    using Opcodes = std::vector<std::pair<std::string, int>>;
 
-    void ReadFile();
+    struct Instruction
+    {
+        Instruction(Opcode o, int s) : op(o), step(s), isVisited(false) {}
+
+        Opcode op;
+        int step;
+        bool isVisited;
+    };
+
+
+    using Instructions = std::vector<Instruction>;
+
     void PrintOpcodes();
+    bool Operation(int& accumulator, Instructions instructions);
+    Instructions ChangeInstruction(int& ip);
+
+    inline Instructions GetInstructions() { return m_instructions; }
 
 private:
-    Opcodes m_opcodes;
+    Instructions m_instructions;
     static constexpr char const *FILENAME = "../src/Calendar2020/Day8/resources/Day8.txt";
+
+    void ReadFile();
+    Opcode StringToOpcode(const std::string& op);
+    std::string OpcodeToString(const Opcode& op);
 };
 
 
